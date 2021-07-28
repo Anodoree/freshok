b037571a59b2307de0da2c309a5028dc412567ec
Anodoree <emfwws@gmail.com>
2021-07-28 23:52:00 +0300

    header under construction

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-  
+
+  $('.catalog__btn').on('click', function() {
+    $('.catalog__list').toggleClass('catalog__list--active');
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+  });
+
 });
